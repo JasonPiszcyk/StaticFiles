470eab822a808be1f70beece27adff5083988eb0
Jason Piszcyk <Jason@Piszcyk.com>
2023-09-07 12:16:17 +0100

    Rewrite to install stackstorm not use containers

--- configure_st2_appliance.sh
+++ configure_st2_appliance.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # Script Name:  configure_st2_appliance
 # Date Written: 5/9/2022
@@ -16,26 +16,14 @@
 ShortOptList="h"
 LongOptList="help:"
 
-# Any Global Variables
-# PROG=$(basename $0)
+# Override PROG as this will be set to a temporary name based on how we are run!
 PROG="configure_st2_appliance"
-LOGFILE=/tmp/${PROG}.log
 
-# File/Directory locations
-ISSUE=/etc/issue
-CLOUD_INIT_DISABLED=/etc/cloud/cloud-init.disabled
-KEYRING_DIR=/etc/apt/keyrings
-DATA_DIR=/data
+# Call in the Iocane core script which has our functions, standard evn variables, etc
+source <(curl -sSL https://raw.githubusercontent.com/JasonPiszcyk/StaticFiles/main/iocane_script_core.sh)
 
-# Docker Info
-DOCKER_KEYRING=${KEYRING_DIR}/docker.gpg
-DOCKER_APT_URL=https://download.docker.com/linux/ubuntu
-DOCKER_GPG_URL=${DOCKER_APT_URL}/gpg
-DOCKER_APT_SRCLIST=/etc/apt/sources.list.d/docker.list
 
 # StackStorm Info
-ST2_DOCKER_COMPOSE_DIR=${DATA_DIR}/st2-docker
-
 
 
 #############################################################################
@@ -67,106 +55,16 @@ __EOF
 
 
 ##############
-# Log - Write a message to the screen + logfile
-##############
-Log()
-{
-  echo -e "$*" | tee -a ${LOGFILE}
-}
-
-
-##############
 # CustomiseEtcIssue - Customise the login screen message
 ##############
 CustomiseEtcIssue()
 {
-  Log "Customising ${ISSUE}"
+  Log -t "Customising ${ISSUE}"
 
   echo "IP Address: \\4" >> ${ISSUE}
   echo "" >> ${ISSUE}
 
-  Log ""
-}
-
-
-##############
-# UpdateAPTCache - Update the APT Cache
-##############
-UpdateAPTCache()
-{
-  Log "Updating the APT cache"
-  apt-get update >> ${LOGFILE} 2>&1
-  if [ $? -ne 0 ]; then
-    Log "ERROR: Unable to update APT cache"
-    exit 1
-  fi
-}
-
-
-##############
-# ApplyUpdates - Apply any outstanding updates to the system
-##############
-ApplyUpdates()
-{
-  Log "Applying any outstanding updates"
-  apt-get -y upgrade >> ${LOGFILE} 2>&1
-  if [ $? -ne 0 ]; then
-    Log "ERROR: Unable to apply updates"
-    exit 1
-  else
-    Log ""
-  fi
-}
-
-
-##############
-# AutoRemovePackages - Automatically remove unused packages
-##############
-AutoRemovePackages()
-{
-  Log "Autoremoving unused packages"
-  apt -y autoremove >> ${LOGFILE} 2>&1
-  if [ $? -ne 0 ]; then
-    Log "ERROR: Unable to remove unused packages"
-    exit 1
-  else
-    Log ""
-  fi
-}
-
-
-##############
-# IsPackageInstalled - Check if a package is installed
-##############
-IsPackageInstalled()
-{
-  package="${1}"
-
-  dpkg -s "${package}" > /dev/null 2>&1
-  [ $? -eq 0 ] && true || false
-}
-
-##############
-# InstallPackage - Install a package
-##############
-InstallPackage()
-{
-  package="${1}"
-
-  Log "Installing Package: ${package}"
-
-  # Is the package already installed?
-  if IsPackageInstalled ${package}; then
-    Log "Package already installed: ${package}"
-  else
-    apt-get -y install ${package} >> ${LOGFILE} 2>&1
-    if [ $? -ne 0 ]; then
-      Log "ERROR: Unable to install package: ${package}"
-      exit 1
-    else
-      Log ""
-    fi
-  fi
+  Log -t ""
 }
 
 
@@ -175,13 +73,13 @@ InstallPackage()
 ##############
 DisableCloudInit()
 {
-  Log "Disabling cloud-init"
+  Log -t "Disabling cloud-init"
   touch ${CLOUD_INIT_DISABLED} >> ${LOGFILE} 2>&1
   if [ $? -ne 0 ]; then
     Log "ERROR: Unable to create file: ${CLOUD_INIT_DISABLED}"
     exit 1
   else
-    Log ""
+    Log -t ""
   fi
 }
 
@@ -191,13 +89,13 @@ DisableCloudInit()
 ##############
 PurgeSnaps()
 {
-  Log "Purging any existing snaps"
+  Log -t "Purging any existing snaps"
   apt purge snapd -y >> ${LOGFILE} 2>&1
   if [ $? -ne 0 ]; then
-    Log "ERROR: Unable to purge snaps"
+    Log -t "ERROR: Unable to purge snaps"
     exit 1
   else
-    Log ""
+    Log -t ""
   fi
 }
 
@@ -207,103 +105,43 @@ PurgeSnaps()
 ##############
 ConfigureFirewall()
 {
-  Log "Setting up basic firewall"
+  Log -t "Setting up basic firewall"
 
   ufw default deny incoming >> ${LOGFILE} 2>&1
   if [ $? -ne 0 ]; then
-    Log "ERROR: Unable to set default UFW policy: Incoming"
+    Log -t "ERROR: Unable to set default UFW policy: Incoming"
     exit 1
   fi
 
   ufw default allow outgoing >> ${LOGFILE} 2>&1
   if [ $? -ne 0 ]; then
-    Log "ERROR: Unable to set default UFW policy: Outgoing"
+    Log -t "ERROR: Unable to set default UFW policy: Outgoing"
     exit 1
   fi
 
   ufw allow ssh >> ${LOGFILE} 2>&1
   if [ $? -ne 0 ]; then
-    Log "ERROR: Unable to set allow SSH incoming in UFW"
+    Log -t "ERROR: Unable to set allow SSH incoming in UFW"
     exit 1
   else
-    Log ""
+    Log -t ""
   fi
 
   ufw disable && ufw --force enable >> ${LOGFILE} 2>&1
   if [ $? -ne 0 ]; then
-    Log "ERROR: Unable to restart UFW"
+    Log -t "ERROR: Unable to restart UFW"
     exit 1
   else
-    Log ""
+    Log -t ""
   fi
 
   ufw status verbose >> ${LOGFILE} 2>&1
   if [ $? -ne 0 ]; then
-    Log "ERROR: Unable to get UFW status"
+    Log -t "ERROR: Unable to get UFW status"
     exit 1
   fi
   ufw status verbose
-  Log ""
-}
-
-
-##############
-# InstallDocker - Install Docker
-##############
-InstallDocker()
-{
-  Log "Setting up Docker"
-
-  # Set up Docker GPG Key
-  Log "Setting up Docker GPG Key"
-
-  install -m 0755 -d ${KEYRING_DIR} >> ${LOGFILE} 2>&1
-  if [ $? -eq 0 ]; then
-    curl -fsSL ${DOCKER_GPG_URL} | gpg --dearmor -o ${DOCKER_KEYRING} >> ${LOGFILE} 2>&1
-    if [ $? -eq 0 ]; then
-      chmod a+r ${DOCKER_KEYRING} >> ${LOGFILE} 2>&1
-      if [ $? -ne 0 ]; then
-        Log "ERROR: Unable to set permissions on docker GPG key"
-        exit 1
-      fi
-    else
-      Log "ERROR: Unable to download docker GPG key"
-      exit 1
-    fi
-  else
-    Log "ERROR: Unable to create GPG key directory"
-    exit 1
-  fi
-
-  # Set up APT repository
-  Log "Setting up Docker APT Repository"
-  cat - << __EOF > ${DOCKER_APT_SRCLIST}
-deb [arch="${pkg_arch}" signed-by=${DOCKER_KEYRING}] ${DOCKER_APT_URL} ${VERSION_CODENAME} stable
-__EOF
-
-  if [ $? -ne 0 ]; then
-    Log "ERROR: Problems setting up Docker APT repository"
-    exit 1
-  fi
-
-  UpdateAPTCache
-
-  # Make sure no docker packages were previusly installed
-  if IsPackageInstalled "docker-ce" || IsPackageInstalled "docker-ce-cli" || IsPackageInstalled "containerd.io" || 
-      IsPackageInstalled "docker-buildx-plugin" || IsPackageInstalled "docker-compose-plugin"; then
-    Log "ERROR: Docker Packages already installed."
-    exit 1
-  fi
-
-  # Install the packages
-  InstallPackage docker-ce 
-  InstallPackage docker-ce-cli 
-  InstallPackage containerd.io 
-  InstallPackage docker-buildx-plugin
-  InstallPackage docker-compose-plugin
-  InstallPackage docker-compose
-
-  Log ""
+  Log -t ""
 }
 
 
@@ -312,24 +150,15 @@ __EOF
 ##############
 InstallStackStorm()
 {
-  Log "Installing StackStorm"
+  Log -t "Installing StackStorm"
 
   # Create the data directory
-  Log "Installing StackStorm Docker Compose Files"
+  Log -t "Installing StackStorm Docker Compose Files"
   install -m 0755 -d ${DATA_DIR} >> ${LOGFILE} 2>&1
   if [ $? -ne 0 ]; then
-    Log "ERROR: Unable to create Data directory"
+    Log -t "ERROR: Unable to create Data directory"
     exit 1
   fi
-
-  # Clone the St2 Docker compose repository
-  cd ${DATA_DIR} && git clone https://github.com/stackstorm/st2-docker >> ${LOGFILE} 2>&1
-  if [ $? -ne 0 ]; then
-    Log "ERROR: Unable to clone StackStorm Docker GIT Repository"
-    exit 1
-  fi
-
-  Log ""
 }
 
 
@@ -342,7 +171,7 @@ InstallStackStorm()
 #
 # Clear the logfile
 #
-[ -e ${LOGFILE} ] && rm -f ${LOGFILE}
+Log -c ""
 
 #
 # Process the command line parameters
@@ -380,30 +209,30 @@ done
 if [ -e /etc/os-release ]; then
   . /etc/os-release
 else
-  Log "ERROR: Unable to determine OS info"
+  Log -t "ERROR: Unable to determine OS info"
   exit 1
 fi
 
 if [ "${NAME}" != "Ubuntu" ]; then
-  Log "ERROR: This must be run on an Ubuntu OS"
+  Log -t "ERROR: This must be run on an Ubuntu OS"
   exit 1
 fi
 
 pkg_arch=$(dpkg --print-architecture)
 if [ $? -ne 0 ]; then
-  Log "ERROR: Unable to determine system architecture"
+  Log -t "ERROR: Unable to determine system architecture"
   exit 1
 fi
 
 # Put some info in the Log file...
-Log ""
-Log "Configuring ST2 appliance"
-Log "=========================="
-Log "OS Name: ${NAME}"
-Log "OS Version: ${VERSION}"
-Log "OS Codename: ${VERSION_CODENAME}"
-Log "OS Package Architecture: ${pkg_arch}"
-Log ""
+Log -t ""
+Log -t "Configuring ST2 appliance"
+Log -t "=========================="
+Log -t "OS Name: ${NAME}"
+Log -t "OS Version: ${VERSION}"
+Log -t "OS Codename: ${VERSION_CODENAME}"
+Log -t "OS Package Architecture: ${pkg_arch}"
+Log -t ""
 
 #
 # Configure /etc/issue
@@ -423,30 +252,33 @@ PurgeSnaps
 #
 # Apply any outstanding updates
 #
-UpdateAPTCache
-ApplyUpdates
+Log -t "Applying Updates"
+if ! UpdateAPTCache ; then
+  Log -t "ERROR: Unable to update APT cache"
+  exit 1
+fi
+  
+if ! ApplyUpdates ; then
+  Log "ERROR: Unable to apply updates"
+  exit 1
+fi
 
 #
 # Make sure the packages we need are installed
 #
-Log "Installing Required Packages"
-Log "-----------------------------"
-InstallPackage "ca-certificates"
-InstallPackage "gnupg"
-InstallPackage "crudini"
-InstallPackage "ufw"
+Log -t "Installing Required Packages"
+Log -t "-----------------------------"
+if ! InstallPackage ca-certificates gnupg crudini ufw ; then
+  Log -t "ERROR: A problem occurred when installed required packages"
+  exit 1
+fi
 
-Log ""
+Log -t ""
 
 #
 # Set up firewall
 #
 ConfigureFirewall
-
-#
-# Install Docker
-#
-InstallDocker
 
 #
 # Install StackStorm
@@ -456,15 +288,27 @@ InstallStackStorm
 #
 # Apply any outstanding updates and remove unused packages
 #
-UpdateAPTCache
-ApplyUpdates
-AutoRemovePackages
+Log -t "Applying Updates and autoremoving unused packages"
+if ! UpdateAPTCache ; then
+  Log -t "ERROR: Unable to update APT cache"
+  exit 1
+fi
+  
+if ! ApplyUpdates ; then
+  Log "ERROR: Unable to apply updates"
+  exit 1
+fi
+
+if ! AutoRemovePackages ; then
+  Log "ERROR: Unable to automatically remove unused packages"
+  exit 1
+fi
 
 # All done
-Log ""
-Log "*************************************************************************"
-Log "* All Done! System should be rebooted to ensure all updates are applied *"
-Log "*************************************************************************"
-Log ""
+Log -t ""
+Log -t "*************************************************************************"
+Log -t "* All Done! System should be rebooted to ensure all updates are applied *"
+Log -t "*************************************************************************"
+Log -t ""
 
 exit 0
---
+++ iocane_script_core.sh
@@ -0,0 +1,136 @@
+#!/usr/bin/env bash
+#
+# Script Name:  iocane_script_core
+# Date Written: 7/9/2022
+# Written By:   Jason Piszcyk
+# Version:      1.0
+# Description:  Core script function, etc
+# Notes:
+#
+# Usage:        source <(curl -sSL https://raw.githubusercontent.com/JasonPiszcyk/StaticFiles/main/iocane_script_core.sh)
+#
+# Copyright (c) 2023 Iocane Pty Ltd
+#
+
+# Standard Environment variables
+PROG=${PROG:-$(basename $0)}
+
+# File/Directory locations
+LOGFILE=/tmp/${PROG}.log
+ISSUE=/etc/issue
+CLOUD_INIT_DISABLED=/etc/cloud/cloud-init.disabled
+KEYRING_DIR=/etc/apt/keyrings
+DATA_DIR=/data
+
+# Date Info
+CUR_DATE=$(${DATE} +"%d/%m/%Y")
+
+# Hostname
+CUR_HOST=$(hostname)
+
+
+#############################################################################
+#
+# Functions
+#
+#############################################################################
+##############
+# Log - Write a message to the logfile (and optionally to the screen)
+##############
+Log()
+{
+  if [ "$1" = "-c" ]; then
+    RemoveFile ${LOGFILE}
+    echo -n "" > ${LOGFILE}
+    shift
+  fi
+
+  if [ "$1" = "-d" ]; then
+    echo -n "$(date +"%H:%M:%S %d/%m/%Y"): " >> ${LOGFILE}
+    shift
+  fi
+
+  if [ "$1" = "-t" ]; then
+    shift
+    echo "$*"
+  fi
+
+  echo "$*" >> ${LOGFILE}
+}
+
+
+#############################################################################
+#
+# File/Directory Manipulation Functions
+#
+#############################################################################
+##############
+# RemoveFile - Remove a file if it exists
+##############
+RemoveFile()
+{
+  file_to_delete="${1}"
+
+  [ -f ${file_to_delete} ] && rm -f ${TMP_FILE} || true
+}
+
+
+
+#############################################################################
+#
+# Package Management Functions
+#
+#############################################################################
+##############
+# UpdateAPTCache - Update the APT Cache
+##############
+UpdateAPTCache()
+{
+  apt-get update
+}
+
+
+##############
+# ApplyUpdates - Apply any outstanding updates to the system
+##############
+ApplyUpdates()
+{
+  apt-get -y upgrade
+}
+
+
+##############
+# AutoRemovePackages - Automatically remove unused packages
+##############
+AutoRemovePackages()
+{
+  apt -y autoremove
+}
+
+
+##############
+# IsPackageInstalled - Check if packages are installed
+##############
+IsPackageInstalled()
+{
+  package_list=$*
+  packages_installed=true
+
+  for pkg in ${package_list}; do
+    dpkg -s "${pkg}" > /dev/null 2>&1
+    [ $? -eq 0 ] || packages_installed=false
+  done
+
+  ${packages_installed}
+}
+
+
+##############
+# InstallPackages - Install packages
+##############
+InstallPackages()
+{
+  package_list=$*
+
+  apt-get -y install ${package_list}
+}

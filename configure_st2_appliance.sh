1479511dcf4eff2ae8f8fd9609bb196af4276095
Jason Piszcyk <Jason@Piszcyk.com>
2023-09-08 08:31:47 +0100

    Removed incorrect install commands

--- configure_st2_appliance.sh
+++ configure_st2_appliance.sh
@@ -226,8 +226,6 @@ __EOF
     exit 1
   fi
 
-apt-get install rabbitmq-server -y --fix-missing
-apt-get install -y 
   if ! InstallPackages erlang-base erlang-asn1 erlang-crypto erlang-eldap erlang-ftp \
         erlang-inets erlang-mnesia erlang-os-mon erlang-parsetools erlang-public-key \
         erlang-runtime-tools erlang-snmp erlang-ssl erlang-syntax-tools erlang-tftp \

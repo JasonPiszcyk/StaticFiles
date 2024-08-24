43a974492d9f216b33b86fe7d30b427f20e75829
Jason Piszcyk <Jason@Piszcyk.com>
2024-08-24 14:55:49 +0930

    Updated RPM info

--- Scripts/prepare_ias_appliance.sh
+++ Scripts/prepare_ias_appliance.sh
@@ -11,6 +11,13 @@
 # shellcheck disable=SC2119,SC2317
 #
 
+# IAS RPM Info
+IAS_RPM_VERSION="0.1.0"
+IAS_RPM_RELEASE="5"
+IAS_RPM_NAME="ias-appliance-${IAS_RPM_VERSION}-${IAS_RPM_RELEASE}.x86_64.rpm"
+IAS_RPM_LOCATION="https://raw.githubusercontent.com/JasonPiszcyk/StaticFiles/main/RPMS"
+
+
 #############################################################################
 #
 # I choose to start the main code.... Here
@@ -31,6 +38,6 @@ dnf install -y epel-release
 dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
 # Install the ias-appliance RPM - Will install all dependant packages
-dnf install -y https://raw.githubusercontent.com/JasonPiszcyk/StaticFiles/main/RPMS/ias-appliance-0.1.0-3.x86_64.rpm
+dnf install -y "${IAS_RPM_LOCATION}/${IAS_RPM_NAME}"
 
 exit 0

2be170b76e59e6ca28384e5c4230b2f59e644b8d
Jason Piszcyk <Jason@Piszcyk.com>
2024-10-15 17:27:24 +1030

    Remove old RPMS - Set script version

--- Scripts/prepare_ias_appliance.sh
+++ Scripts/prepare_ias_appliance.sh
@@ -12,7 +12,7 @@
 #
 
 # IAS RPM Info
-IAS_RPM_NAME="ias-appliance-${IAS_RPM_VERSION:-"0.6.1-5"}.x86_64.rpm"
+IAS_RPM_NAME="ias-appliance-${IAS_RPM_VERSION:-"0.6.1-8"}.x86_64.rpm"
 IAS_RPM_LOCATION="https://raw.githubusercontent.com/JasonPiszcyk/StaticFiles/main/RPMS"
 
 
